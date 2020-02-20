d4359fcb02245f26c95e6ca1be91f133b4d7a4ce
wylieza <wylieza@gmail.com>
2020-02-20 21:20:06 +0200

    implemeted modification from part c

--- matlabassp2.m
+++ matlabassp2.m
@@ -1,9 +1,27 @@
 disp("flsin function");
+graphics_toolkit("gnuplot");
 
 function flsin = f(a, w, p, ni, nf) %flsin is the variable returned, f is the name of the function
 x = ni:1:nf
 flsin = a*sin(w*x + p)
 end
 
-y = f(1, 2, 0, 0, 10)
-plot(0:1:10, y)
+function [indicies, values] = flsinB(a, w, p, ni, nf)
+  indicies = ni:1:nf;
+  values = a*sin(w*indicies + p);
+end
+
+y1 = f(1, 2, 0, 0, 10)
+plot(0:1:10, y1)
+
+figure();
+y2 = f(5, 0.5, 2, 0, 10)
+plot(0:1:10, y2)
+
+figure();
+y3 = f(1, 2, 0, 5, 15)
+plot(0:1:10, y3)
+
+figure();
+[ind, val] = flsinB(1, 2, 0, 3, 13);
+plot(ind, val);
