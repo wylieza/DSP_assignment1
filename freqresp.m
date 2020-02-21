3743da2a45e310a52170ff91c4f097a6fcf9738e
wylieza <wylieza@gmail.com>
2020-02-21 19:30:55 +0200

    freqresp working and handin4 modified to get final figures

--- freqresp.m
+++ freqresp.m
@@ -1,3 +1,7 @@
 function H = freqresp(h, omega)
+  offset = 11; %Offset because assuming h[1] is actually h[-10]
+  H = 0;
   for n = 1:1:length(h)
-    
+    H += h(n)*exp(-j*omega*(n-offset));
+  end
+end
--- handin4.m
+++ handin4.m
@@ -6,7 +6,7 @@ rf = 70;
 global arrn = ri:1:rf;
 range = length(arrn);
 
-W = 2*pi/5;
+W = pi/10;
 
 function hofx = hof(x)
   hofx = 0;
@@ -15,9 +15,23 @@ function hofx = hof(x)
   end
 end
 
+function htofn = htof(n)
+  htofn = 0;
+  if (n >= 0 && n <=3)
+    htofn = (n+1)/16;
+  elseif (n >= 4 && n <=6)
+    htofn = (7-n)/16;
+  end
+end
+
 h = zeros(range);
 for n = arrn
   h(n -ri+1) = hof(n);
+end
+
+ht = zeros(range);
+for n = arrn
+  ht(n -ri+1) = htof(n);
 end
 
 function Hofw = Hof(w)
@@ -29,5 +43,24 @@ function Hofw = Hof(w)
   end
 end
 
+disp("Manual");
 disp(abs(Hof(W)));
 disp(arg(Hof(W)));
+
+disp("Direct");
+disp(abs(freqresp(ht, W)))
+disp(arg(freqresp(ht, W)))
+
+arrW = 0:0.01:4*pi;
+arrH = [];
+
+for w = arrW
+  arrH = [arrH, freqresp(ht, w)];
+end
+
+subplot(2,1,1);
+plot(arrW, abs(arrH));
+subplot(2,1,2);
+plot(arrW, arg(arrH));
+
+%print(gcf, '-dpng', 'handin4imporved.png') %Save as png
